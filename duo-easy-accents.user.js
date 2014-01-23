90caf9fc61bb397d8c7c3f292ae6fca76538a0b8
HodofHod <a.mishulovin@gmail.com>
2014-01-22 23:07:35 -0600

    Added Romanian, Hungarian, Dutch, and Turkish. Runs on https too now.

--- duo-easy-accents.user.js
+++ duo-easy-accents.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Duolingo - Easy Accents
 // @description  Makes typing characters with accents and diacritics easy! Just use the Alt key!
-// @match        http://www.duolingo.com/*
+// @match        *://www.duolingo.com/*
 // @author       @HodofHod
-// @version      0.1.0
+// @version      0.1.1
 // ==/UserScript==
 
 
@@ -37,17 +37,27 @@ function main(){
             'A': ['ã', 'á', 'â', 'à'], 'E': ['é', 'ê'], 'I': ['í'],
             'O': ['õ', 'ó', 'ô'], 'U': ['ú', 'ü'], 'C': ['ç']
         },
-        german_map = {
-            'A': 'ä', 'O': 'ö', 'U': 'ü', 'S': 'ß'
+        german_map = {'A': 'ä', 'O': 'ö', 'U': 'ü', 'S': 'ß'
         },
         italian_map = {
             'A': ['à', 'á'], 'E': ['è', 'é'], 'I': ['ì', 'í'], 
             'O': ['ò', 'ó'], 'U': ['ù', 'ú']
         },
         polish_map = {'A': 'ą', 'C': 'ć', 'E': 'ę', 'L': 'ł', 
-                      'N': 'ń', 'O': 'ó', 'S': 'ś', 'Z': ['ź','ż']},
-        maps = {'es': spanish_map, 'fr': french_map,  'pt': portuguese_map,
-                'de': german_map,  'it': italian_map, 'pl': polish_map},
+                      'N': 'ń', 'O': 'ó', 'S': 'ś', 'Z': ['ź','ż']
+        },
+        romanian_map = {'A':['ă','â'], 'I': 'î', 'S': ['ş','ș'], 'T': ['ţ','ț']
+        };
+        hungarian_map = {'A': 'á', 'E': 'é', 'I': 'í', 'O': ['ö','ó','ő'], 'U': ['ü','ú','ű']
+        },
+        dutch_map = {'E': ['é','ë'], 'I': 'ï', 'O': ['ó','ö'], 'U': 'ü'
+        },
+        turkish_map = {'C': 'ç', 'G': 'ğ', 'I': ['ı','İ'], 'O': 'ö', 'S': 'ş', 'U': 'ü'
+        },
+        maps = {'es': spanish_map, 'fr': french_map, 'pt': portuguese_map,
+                'it': italian_map, 'de': german_map, 'hu': hungarian_map,
+                'tr': turkish_map, 'pl': polish_map, 'ro': romanian_map,
+                'dn': dutch_map},
         taps = 0,
         last_press = [];
 
@@ -59,7 +69,7 @@ function main(){
         //Get the list of characters that the pressed key could be replaced with
         var char_lst = maps[e.target.lang][String.fromCharCode(e.which)];
         //If the pressed key doesn't have accented alternates, return
-        if (char_lst === undefined){ return;}
+        if (char_lst === undefined){ return false; }
         //Choose which accented character based on the number of taps
         var chr = char_lst[(taps-1) % char_lst.length],
             //save the old text
