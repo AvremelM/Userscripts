d32c0ba881557b1f0d50a8d24d6fa8eeb39c3011
HodofHod <a.mishulovin@gmail.com>
2014-01-29 14:37:54 -0600

    Create duo-mods.user.js

---
+++ duo-mods.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Duolingo Comment Links
+// @description  Turn comment timestamps into direct links
+// @match        *://www.duolingo.com/*
+// @author       HodofHod
+// @namespace    HodofHod
+// @version      0.0.1
+// ==/UserScript==
+
+
+function inject(f) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.textContent = '(' + f.toString() + ')()';
+    document.body.appendChild(script);
+}
+
+inject(function(){
+    $(document).on('mouseover', '.discussion-comments-list:not(.dlinked)', function (){
+        $(this).addClass('dlinked');    
+        $('li[id*=comment-] .body').each(function(){
+            var $timestamp = $(this).next('.footer').contents().filter(function(){return this.nodeType === 3;}),
+                $link = $('<a href="' + document.location.pathname.replace(/\$.+$/, '') +
+                            '$comment_id=' + this.id.replace(/^body-(\d+)$/, '$1') +
+                            '">' + $timestamp.text() + '</a>');
+            $timestamp.replaceWith($link);
+        });
+    });
+});
