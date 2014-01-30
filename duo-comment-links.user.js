65ec217f38d4511647602057a7965a409483f35b
HodofHod <a.mishulovin@gmail.com>
2014-01-29 22:42:52 -0600

    Prep for duo-mods

--- duo-comment-links.user.js
+++ duo-comment-links.user.js
@@ -4,19 +4,20 @@
 // @match        *://www.duolingo.com/*
 // @author       HodofHod
 // @namespace    HodofHod
-// @version      0.0.1
+// @version      0.0.2
 // ==/UserScript==
 
 
 function inject(f) {
     var script = document.createElement('script');
     script.type = 'text/javascript';
+    script.name = 'comment_links';
     script.textContent = '(' + f.toString() + ')()';
     document.body.appendChild(script);
 }
 
 inject(function(){
-    $(document).on('mouseover', '.discussion-comments-list:not(.dlinked)', function (){
+    $(document).on('mouseover.hh', '.discussion-comments-list:not(.dlinked)', function (){
         $(this).addClass('dlinked');    
         $('li[id*=comment-] .body').each(function(){
             var $timestamp = $(this).next('.footer').contents().filter(function(){return this.nodeType === 3;}),
