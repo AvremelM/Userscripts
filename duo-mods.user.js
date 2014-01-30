72e91c1b9341d35439e67fc9557cba44f45d969e
HodofHod <a.mishulovin@gmail.com>
2014-01-29 23:18:27 -0600

    Adds settings to turn scripts on and off.

--- duo-mods.user.js
+++ duo-mods.user.js
@@ -4,19 +4,62 @@
 // @match        *://www.duolingo.com/*
 // @author       HodofHod
 // @namespace    HodofHod
-// @version      0.0.1
+// @version      0.1.0
 // ==/UserScript==
 
-
-function addScript(URL) {
-  var script = document.createElement("script");
-  script.setAttribute("src", URL);
-  document.body.appendChild(script);
+function inject(f) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.textContent = '(' + f.toString() + ')()';
+    document.head.appendChild(script);
 }
 
-if (document.location.host === 'www.duolingo.com'){
-    var base = '//github.com/HodofHod/Userscripts/raw/master/';
-    addScript(base+'duo-easy-accents.user.js');
-    addScript(base+'duolingo-lesson-review.user.js');
-    addScript(base+'duo-comment-links.user.js');
+inject(main);
+
+function main(){
+    //modified from developer.mozilla.org/en-US/docs/Web/API/document.cookie
+    function getCookie(sKey) {
+        return unescape(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    }
+    function setCookie(sKey, sValue) {
+        document.cookie = escape(sKey) + "=" + escape(sValue) + 
+        "; expires=Fri, 31 Dec 9999 23:59:59 GMT; domain=duolingo.com; path=/";
+    }
+    function addScript(URL, name) {
+        if (getCookie(name) === 'false' || $('script[name='+name+']').length){return false;}
+        var script = document.createElement("script");
+        script.setAttribute("src", URL);
+        script.setAttribute("name", name);
+        document.head.appendChild(script);
+    }
+    function b(){
+        var base = '//github.com/HodofHod/Userscripts/raw/master/';
+        addScript(base+'duo-easy-accents.user.js', 'easy_accents');
+        addScript(base+'duolingo-lesson-review.user.js', 'lesson_review');
+        addScript(base+'duo-comment-links.user.js', 'comment_links');
+    }
+    b();
+    
+    var mods = ['Lesson Review', 'Easy Accents', 'Comment Links'];
+    $(document).on('mouseover', '#app.settings', function(){
+        if (!$('#mod-settings').length){
+            console.log('modded');
+            template = $('.radio-buttons:last').parents('li').after('<hr>');
+            $.each(mods, function(){
+                var esc_name = this.toLowerCase().replace(/ /g, '_'),
+                    innerHtml = template.clone().html().replace(/sound_effects/g, esc_name)
+                                                 .replace('Sound effects', this),
+                    $setting = $('<li>'+innerHtml+'</li>').insertAfter(template),
+                    cookie = getCookie(esc_name),
+                    which = (cookie === "true" || !cookie ? 'first' : 'last');
+                $setting.find('input:radio:' + which).prop('checked', true);
+                $setting.find('input:radio').on('change', function(e){
+                    var on = /_on$/.test(e.target.id);
+                    setCookie(esc_name, on), b();
+                    !on && $('script[name='+esc_name+']').remove() && (duo.js_version='');
+                });
+            });
+            template.after('<hr id="mod-settings">');
+        }
+    });
 }
