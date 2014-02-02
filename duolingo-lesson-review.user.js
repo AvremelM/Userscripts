e2f3aaf0be50ad81e4cd9e458d09465343cf1fba
HodofHod <a.mishulovin@gmail.com>
2014-02-01 23:38:01 -0600

    Major rewrite. Should fix most bugs. Added sounds, commenting, and voting.

--- duolingo-lesson-review.user.js
+++ duolingo-lesson-review.user.js
@@ -4,18 +4,11 @@
 // @match        *://www.duolingo.com/*
 // @author       HodofHod
 // @namespace    HodofHod
-// @version      0.1.5
+// @version      0.1.6
 // ==/UserScript==
 
-//Beware all who enter here. This code may be hideous and worse.
-//I am not responsible for any damage done to your eyes, or the ears of those around you.
-
-//TODO: Inject a stylesheet and use classes instead of .css()
-//      Maybe get really fancy, and hijack Session and SessionView, and switch lessons with .set({positon,x}) and .render()
-//          Needs work. Doesn't save grading. Replays wrong audio.
-//Fix: Switching from successview before graphs are loaded, breaks graphs, and logs infinite errors. 
-//          hide() instead of .detach() works, but it breaks sound and other things on non end lessons.
-//      
+//TODO: Inject a stylesheet and use classes instead of .css()?
+//      Don't disable report when going to last lesson from failview.
 
 function inject(f) { //Inject the script into the document
     var script;
@@ -25,7 +18,6 @@ function inject(f) { //Inject the script into the document
     script.textContent = '(' + f.toString() + ')(jQuery)';
     document.head.appendChild(script);
 }
-
 
 inject(init);
 
@@ -60,12 +52,10 @@ function init(){
     }
 
     function main(){
-        console.log('main');
         cleanup();
-        add_arrows();
         
-        var lessons = {},
-            cur_lesson_id = 1,
+        lessons = {};
+        var cur_lesson_id = 1,
             selected_lesson_id = cur_lesson_id,
             finished = false,
             failed = false,
@@ -74,53 +64,56 @@ function init(){
             redesign = duo.user.get("ab_options").site_redesign_experiment ;
         
         function save_lesson(save_id){
-            var app_class = $('#app').prop('class');
-            lessons[save_id] = redesign ? [$('.player-container>footer, .player-main').clone(), $('#discussion-modal-container').clone(), $('#app').prop('class')]
+            var $problem = $('.player-main'),
+                $controls = $('.player-container>footer'),
+                $discussion = $('#discussion-modal-container');
+            
+            lessons[save_id] = redesign ? [$problem.add($controls), $discussion, $('#app').prop('class')]
                                         : [$('#app>div:not(.player-header)').clone(), $('#app').prop('class')];
+            if (redesign && save_id !== 'end'){
+                lessons[save_id][0] = $problem.after($problem.clone()).add($controls.clone()).detach();
+                lessons[save_id][1] = !failed ? $discussion.detach() : $discussion;
+                var l = lessons[save_id][0].add(lessons[save_id][1]),
+                    resume_button = $('<button id="resume_button" class="btn success large right" tabindex="20">Resume</button>');
+            
+                //Disable or hide all controls except for discuss
+                l.find('#show-report-options').attr('disabled', 'disabled');
+                l.find('#submit_button, #skip_button, #home-button, #fix_mistakes_button').hide();
+                l.find('#next_button, #continue_button, #retry-button')
+                    .after($('#resume_button').length ? null : resume_button).hide();
+            }
         }
-
+        
         function replace_lesson(replace_id){
             console.log('cur: ' + cur_lesson_id + ', old id: ' + selected_lesson_id + ', new id ' + replace_id);
             if (selected_lesson_id === replace_id){ return false; } //don't replace yourself with yourself.
             if (selected_lesson_id === cur_lesson_id || selected_lesson_id === 'end'){//if switching away from cur_lesson
-                lessons[cur_lesson_id] = redesign ? [$('.player-container>footer, .player-main, #end-carousel').detach(), $('#discussion-modal-container').detach(), $('#app').prop('class')]
-                                                  : [$('#app>div:not(.player-header)').detach(), $('#app').prop('class')];
+                lessons[cur_lesson_id] = redesign ? [$('.player-main, #end-carousel').hide()
+                                                        //detach footer to prevent #discussion-toggle conflicts
+                                                        .add($('.player-container>footer').detach()),
+                                                     $('#discussion-modal-container').detach(), 
+                                                     $('#app').prop('class')]
+                                                  : [$('#app>div:not(.player-header)').hide(), 
+                                                     $('#app').prop('class')];
             }else if (lessons[replace_id] === undefined){
                 return false;
             }else{
-                redesign ? $('#controls, .player-main, .discussion-modal-container').remove()
-                		 : $('#app>div:not(.player-header)').remove();
+                redesign ? $('.player-container>footer, .player-main, #discussion-modal-container').detach()
+                         : $('#app>div:not(.player-header)').remove();
             }
             
             var l = lessons[replace_id][0];
-            $(redesign?'.player-container':'#app').append(l);
-            redesign && $('body').append(lessons[replace_id][1]);
+            $(redesign?'.player-container':'#app').append(l.show());
+            redesign && $('body').append(lessons[replace_id][1]);//discussions
             $('#app').prop('class', lessons[replace_id][redesign+1]);//tricky trick
-            bind_discussion_toggle();//TODO: Remove the other?
                 
-            $('.token-wrapper:has(.non-space)').hover(function(){
-                var a = $(this).outerWidth(),
-                    b = $(this).find('.hint-table').outerWidth(),
-                    c = (b / 2) - (a / 2);
-                $(this).css('position','relative').find('.hint-table').toggle().css({left: -1*c+'px', top: '15px'});
-            });
-
-            //Disable all controls except for discuss
-            if (replace_id !== cur_lesson_id){//switching not to cur
-                l.find('#next_button, #continue_button, #show-report-options, #discussion-modal textarea').attr('disabled', 'disabled');
-                l.find('#submit_button, #skip_button, .twipsy\
-                   #home-button, #fix_mistakes_button, #controls .tooltip').remove();
-                l.find('.hint-table').hide();
-                var resume_button = $('<button id="resume_button" class="btn success large right" tabindex="20">Resume</button>');
-                l.find('#next_button, #continue_button, #retry-button').after(resume_button).remove();
-                
-                if (finished && !failed){
-                    $(redesign?'.player-container':'#app').prepend(header);
-                }
-            }else if (finished && !failed){//if switching to end of successful lesson, remove header.
-                $('.player-header').detach();
-            }else{ $('.twipsy').hide(); }
+            $('.hint-table, .twipsy, #controls .tooltip').hide();    
+            if (finished && !failed){//Add or remove header as necessary, since success endview removes it.
+                (replace_id !== cur_lesson_id) ? $(redesign?'.player-container':'#app').prepend(header)
+                                               : $('.player-header').detach();
+            }
             
+            bind_discussion_toggle();//TODO: Remove the other?
             selected_lesson_id = replace_id;
             select_cell(selected_lesson_id);
         }
@@ -141,11 +134,11 @@ function init(){
 
         //Hijack the rendered() function (it runs after each problem is rendered)
         duo.SessionView.prototype.rendered = function newRendered(){
-            console.log('render');
             if ($('#prev-arrow, #next-arrow').length !== 2){
                 add_arrows();
             }
             select_cell(selected_lesson_id);
+            preloadDiscussions();
             return origRendered.apply(this, arguments);
         };
 
@@ -153,7 +146,6 @@ function init(){
         duo.SessionView.prototype.next = function newNext(){
             if ($('#discussion-toggle').size() && !$("#discussion-modal").size()){
                 waiting_for_discussion_load = true;
-                console.log('wait for discussions to finish loading');
             } else {
                 cur_lesson_id = (this.model.get('position') + 1); //0-indexed
                 save_lesson(cur_lesson_id);
@@ -172,10 +164,11 @@ function init(){
                     .css('background-image', '-moz-linear-gradient(center top , #EE6969, #FF0000)')
                     .css(redesign ? {} : {'border': '1px solid #950000', 'border-left':'none'});
             }
-        }
+        };
         function bind_discussion_toggle(){
-            $('#discussion-toggle').on('click', function(e){
+            $('#discussion-toggle').off().on('click', function(e){
                 e.stopImmediatePropagation();
+                $('#discussion-modal-container').show();
                 $('#discussion-modal').modal('show');
                 $('.close-modal-background, .modal-backdrop, .player-discussion>.close').on('click', function(){
                     $('#discussion-modal').modal('hide');
@@ -184,58 +177,37 @@ function init(){
         }
         
         //Load the discussions so they're saved by the cloning.
-        document.body.addEventListener('DOMNodeInserted', function (e) {
-            if (redesign ? $(e.target).has('#discussion-toggle').length && e.target.id !== 'controls'
-                           : e.target.id === 'discussion-modal-container'){
-                $('<style type="text/css" id="modalcss">.modal-backdrop.in{display:none;}</style>').appendTo("head");
-                $('#discussion-modal-container').hide();
-                console.log('load discussions');
-                $('#discussion-toggle').click();
-                bind_discussion_toggle();
-                document.body.addEventListener('DOMNodeInserted', function wait() {
-                    if  ($('.modal-backdrop.in').length){
-                        $('#discussion-modal').modal('toggle')
-                        $('#discussion-modal-container').show();
-                        
-                        $('.modal-backdrop.in, #modalcss').remove();
-                        if (waiting_for_discussion_load === true){
-                            waiting_for_discussion_load = false;
-                            $('#next_button, #continue_button').click();
-                        }
-                        document.body.removeEventListener('DOMNodeInserted', wait);
+        function preloadDiscussions(){
+            document.body.addEventListener('DOMNodeInserted', function waitForToggle(e) {
+                if (redesign ? $(e.target).has('#discussion-toggle').length && e.target.id !== 'controls'
+                               : e.target.id === 'discussion-modal-container'){
+                    $('<style type="text/css" id="modalcss">.modal-backdrop{display:none;}</style>').appendTo("head");
+                    $('#discussion-modal-container').hide();
+                    $('#discussion-toggle').click();
+                    bind_discussion_toggle();
+                    document.body.removeEventListener('DOMNodeInserted', waitForToggle);
+                }
+            });
+            document.body.addEventListener('DOMNodeInserted', function waitForDiscussions() {
+                if  ($('.modal-backdrop.in').length){
+                    $('#discussion-modal').modal('toggle');
+                    $('.modal-backdrop.in, #modalcss').remove();
+                    if (waiting_for_discussion_load === true){
+                        waiting_for_discussion_load = false;
+                        $('#next_button, #continue_button').click();
                     }
-                });
-            }else if (finished && $(e.target).has('.session-end-footer').length){
-                finish();
-            }
-        });
-        
-        $(document).on('click.hh', 'li[id^=element-]', function(){
-            var clicked_id = parseInt(this.id.replace('element-', ''), 10);
-            if (lessons[clicked_id] !== undefined){
-                if (failed){ console.log('fail click'); $('#review_button').click(); }
-                replace_lesson(clicked_id);
-            }
-        });
-        
-        
-        $(document).on('click.hh', '#resume_button', function(){ 
-            replace_lesson(cur_lesson_id); 
-            //TODO: See if this is necessary after the redesign.
-            if (finished && ($('#end-carousel .left').length > 1 || !$('#end-carousel .active').length)){
-                $('#end-carousel .item').removeClass('active next left');
-                $('#end-carousel .item:eq(' +$('.' + (redesign ? 'carousel-' : '') + 'dots .active').data('slide')+ ')').addClass('active');
-                $('#end-carousel .item.active').is(':last-child') && $(".carousel").carousel("pause");
-            }
-        });
+                    document.body.removeEventListener('DOMNodeInserted', waitForDiscussions);
+                }
+            });
+        }
         
         function finish(){
             var button = $('<button id="review_button" class="btn large btn-lg btn-standard right" style="margin:0 10px 0 0;">Review</button>'),
                 loc = failed ? '#controls>' + (redesign ? '.col-right' : '.row>.right') 
                              : (redesign ? '.session-end-footer' : 'div.buttons');
             failed ? $(loc).append(button) : $(loc).prepend(button);//Redesign CAN both be prepend
-            !failed && redesign ? button.css({position:'absolute', left:'40px'}) 
-                                : button.css('float','left');
+            button.css(!failed && redesign ? {position:'absolute', left:'40px'}
+                                           : {float: 'left'});                    
             finished = true;
             last_lesson_id = cur_lesson_id;
             cur_lesson_id = 'end';
@@ -244,13 +216,10 @@ function init(){
         }
         
         duo.SessionView.prototype.showFailView = function(){
-            if ($('#discussion-toggle').size() && !$("#discussion-modal").size()){
-                console.log('wait for discussions to finish loading');
-                waiting_for_discussion_load = true;//TODO
-            }
+            failed = true;
+            bind_discussion_toggle();
             save_lesson(cur_lesson_id);
             r = origShowFailView.apply(this, arguments);
-            failed = true;
             finish();
             $('.close-fail').hide();
             add_arrows();
@@ -262,20 +231,38 @@ function init(){
             cur_lesson_id -= !finished; //newNext() increments on the last one too, adjust for that.
             header = $('.player-header').detach();//TODO: Improve? 
             origShowEndView.apply(this, arguments);
-            finished = true; //TODO FIX THIS ONCE THE MIGRATION TO THE REDESIGN IS COMPLETE.
-            //finish(); end view won't fully load by here. Instead, I'm adding an else if to the DOMNodeInserted binding
+            //end view won't have loaded. Wait for mouseover on end app, where the footer exists.
+            $(document).on('mousemove', '#app.slide-session-end', function(){
+                if ($('.session-end-footer').length){ 
+                    finish(); 
+                    $(document).off('mousemove', '#app.slide-session-end');
+                }
+            });
         };
         
         duo.TimedSessionView.prototype.timesUp = function(){
             save_lesson(cur_lesson_id);
             finished = true;
             origTimesUp.apply(this, arguments);
-        }
-            
-        $(document).on('click.hh', '#review_button', function(){
-            if (failed){
-                save_lesson('end');
+        };
+        
+        $(document).on('click.hh', 'li[id^=element-]', function(){
+            var clicked_id = parseInt(this.id.replace('element-', ''), 10);
+            if (lessons[clicked_id] !== undefined){
+                replace_lesson(clicked_id);
             }
+        });
+        
+        $(document).on('click.hh', '#resume_button', function(){ 
+            replace_lesson(cur_lesson_id); 
+            if (finished && ($('#end-carousel .left').length > 1 || !$('#end-carousel .active').length)){
+                $('#end-carousel .item').removeClass('active next left');
+                $('#end-carousel .item:eq(' +$('.' + (redesign ? 'carousel-' : '') + 'dots .active').data('slide')+ ')').addClass('active');
+                $('#end-carousel .item.active').is(':last-child') && $(".carousel").carousel("pause");
+            }
+        });    
+        
+        $(document).on('click.hh', '#review_button', function(){
             replace_lesson(last_lesson_id);
         });
         
@@ -311,14 +298,10 @@ function init(){
         }
         
         function activate_arrows(){
-            selected_lesson_id > 1 || selected_lesson_id === 'end' ? activate($('#prev-arrow')) : deactivate($('#prev-arrow'));
-            selected_lesson_id !== cur_lesson_id ? activate($('#next-arrow')) : deactivate($('#next-arrow'));
-            function activate($arrow){
-                $arrow.css({'background-position':'-323px -178px',cursor:'pointer','pointer-events':''});
-            }
-            function deactivate($arrow){
-                $arrow.css({'background-position':'-323px -130px',cursor:'initial','pointer-events':'none'});
-            }
+            var active   = {'background-position':'-323px -178px',cursor:'pointer','pointer-events':''},
+                inactive = {'background-position':'-323px -130px',cursor:'initial','pointer-events':'none'};
+            $('#prev-arrow').css( (selected_lesson_id > 1 || selected_lesson_id === 'end') ? active : inactive);
+            $('#next-arrow').css( (selected_lesson_id !== cur_lesson_id)                   ? active : inactive);
         }
     }
 }
